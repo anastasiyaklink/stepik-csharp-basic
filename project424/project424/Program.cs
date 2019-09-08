fd6046d9f647740d8a639f95d93450e2ca5e09c6
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-09 02:13:14 +0300

    424: координатная четверть

---
+++ project424/project424/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace project424
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            if (x > 0)
+            {
+                if (y > 0)
+                {
+                    Console.WriteLine("I");
+                }
+                else
+                {
+                    Console.WriteLine("IV");
+                }
+
+
+            }
+            else
+            {
+                if(y > 0)
+                {
+                    Console.WriteLine("II");
+                }
+                else
+                {
+                    Console.WriteLine("III");
+                }
+            }
+
+        }
+    }
+}
