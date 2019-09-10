9207eb3e929f12a9db8b20a00bc05c1e5919b984
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-11 01:19:48 +0300

    432 проверка решения (в уме)

---
+++ test432/test432/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace test432
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = 30;
+            int b = 6;
+            a = a / 2 * b;
+            int c;
+            if (a > b)
+            {
+                c = a - 3 * b;
+            }
+            else
+            {
+                c = a + 3 * b;
+            }
+
+            Console.WriteLine(c);
+        }
+    }
+}
