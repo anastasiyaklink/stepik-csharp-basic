78458b91bec74b23e5ff559cd957e810382d9848
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-10 17:34:34 +0300

    426 Дни в месяце swich case

---
+++ project426/project426/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace project426
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string m = Console.ReadLine();
+            switch (m)
+            {
+                case "1": Console.WriteLine("31"); break;
+                case "2": Console.WriteLine("29"); break;
+                case "3": Console.WriteLine("31"); break;
+                case "4": Console.WriteLine("30"); break;
+                case "5": Console.WriteLine("31"); break;
+                case "6": Console.WriteLine("30"); break;
+                case "7": Console.WriteLine("31"); break;
+                case "8": Console.WriteLine("31"); break;
+                case "9": Console.WriteLine("30"); break;
+                case "10": Console.WriteLine("31"); break;
+                case "11": Console.WriteLine("30"); break;
+                case "12": Console.WriteLine("31"); break;
+            }
+        }
+    }
+}
