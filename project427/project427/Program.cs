e58e27d790f743406d774ed4219467669f002190
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-11 01:21:09 +0300

    Update Program.cs

--- project427/project427/Program.cs
+++ project427/project427/Program.cs
@@ -6,7 +6,22 @@ namespace project427
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string m = Console.ReadLine();
+            switch (m)
+            {
+                case "1": Console.WriteLine("Зима"); break;
+                case "2": Console.WriteLine("Зима"); break;
+                case "3": Console.WriteLine("Весна"); break;
+                case "4": Console.WriteLine("Весна"); break;
+                case "5": Console.WriteLine("Весна"); break;
+                case "6": Console.WriteLine("Лето"); break;
+                case "7": Console.WriteLine("Лето"); break;
+                case "8": Console.WriteLine("Лето"); break;
+                case "9": Console.WriteLine("Осень"); break;
+                case "10": Console.WriteLine("Осень"); break;
+                case "11": Console.WriteLine("Осень"); break;
+                case "12": Console.WriteLine("Зима"); break;
+            }
         }
     }
 }
