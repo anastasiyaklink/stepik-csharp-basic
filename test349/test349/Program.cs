b8ec6df9e4f6b29050e6a981672fefb7e43e5287
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-11 01:18:57 +0300

    349 проверка решения в уме

---
+++ test349/test349/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace test349
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            
+        }
+    }
+}
