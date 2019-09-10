d69f0f46c3c759a08bca5fc9cc745a4d2f26f150
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-10 20:13:49 +0300

    435 Задачи Васи Math.Ceiling

---
+++ test435/test435/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace test435
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double k = Convert.ToDouble(Console.ReadLine());
+            double m = Convert.ToDouble(Console.ReadLine());
+            double n = Math.Ceiling(k / m);
+            Console.WriteLine(n);
+
+        }
+    }
+}
