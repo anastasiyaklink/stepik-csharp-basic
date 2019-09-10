a1f99147c89aa2e2b5b501496a97c1fba2cb2424
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-10 20:30:59 +0300

    436 ученики и парты

---
+++ test436/test436/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace test436
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Double a = Convert.ToDouble(Console.ReadLine());
+            Double b = Convert.ToDouble(Console.ReadLine());
+            Double c = Convert.ToDouble(Console.ReadLine());
+            Double desk = (Math.Ceiling(a / 2) + Math.Ceiling(b / 2) + Math.Ceiling(c / 2));
+            Console.WriteLine(desk);
+        }
+    }
+}
