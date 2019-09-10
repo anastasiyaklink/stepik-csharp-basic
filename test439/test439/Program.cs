c6f0d66771fe787e8b4089fb6c5713909d3082ef
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-11 01:27:46 +0300

    Update Program.cs

--- test439/test439/Program.cs
+++ test439/test439/Program.cs
@@ -11,8 +11,8 @@ namespace test439
             int b1 = Convert.ToInt32(Console.ReadLine());
             int b2 = Convert.ToInt32(Console.ReadLine());
             if ((Math.Abs(a1 - b1) == 1 && Math.Abs(a2 - b2) == 1) ||
-                a1 == b1 && (Math.Abs(a2 - b2)==1) ||
-                (a2 == b2) && (Math.Abs(a1-b1)==1))
+                a1 == b1 && (Math.Abs(a2 - b2) == 1) ||
+                (a2 == b2) && (Math.Abs(a1 - b1) == 1))
             {
                 Console.WriteLine("YES");
             }
