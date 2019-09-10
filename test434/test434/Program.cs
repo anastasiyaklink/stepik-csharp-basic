152eb3b97bbfc54544ca9a10652b1b06ff0b3289
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-10 18:05:38 +0300

    434 След четное за четным

---
+++ test434/test434/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace test434
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int n1 = (n + 1);
+            if (n1 % 2 == 0)
+            {
+                Console.WriteLine(n1);              
+            }
+            else
+            {
+                Console.WriteLine(n1 + 1);
+            }
+        }
+    }
+}
