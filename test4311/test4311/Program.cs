6186646ffdcd90b040360a1ae6c6ebd70c970f05
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-11 01:52:11 +0300

    4311 возрастающая последовательность

---
+++ test4311/test4311/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace test4311
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int n1 = N / 100;
+            int n2 = N % 100 / 10;
+            int n3 = N % 10;
+            if ((n1 < n2) && (n2 < n3))
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
