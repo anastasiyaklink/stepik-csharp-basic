e502de616a923901ead35eb125dc8683b91a571b
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-10 23:22:49 +0300

    437 последняя цифра числа четная

---
+++ test437/test437/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace test437
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = x % 10;
+            if (y % 2 == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
