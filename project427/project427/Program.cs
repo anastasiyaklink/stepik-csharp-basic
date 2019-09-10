3b34c409e67d84ffebc48a3a4dd110b8d3fc064c
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-10 17:44:21 +0300

    427 время года Swich case

---
+++ project427/project427/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace project427
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
