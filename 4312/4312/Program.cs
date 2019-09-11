e86f092e30a8b275bf037fe04f88f9e33baeb143
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-11 23:16:54 +0300

    test4312 невырожденный треугольник

---
+++ 4312/4312/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Application
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            if (a < (b + c) && b < (a + c) && c < (a + b))
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
