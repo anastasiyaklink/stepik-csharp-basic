63d807ca63d5a1547a1faaaa68d23f63c5a6b1b3
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-11 23:32:32 +0300

    test4313 симметричное число

---
+++ test4313/test4313/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace test4313
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string abcd = Console.ReadLine();
+            if (abcd[0] == abcd[3] && abcd[1] == abcd[2])
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
