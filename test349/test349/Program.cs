3c37032854a89b4d40d39c0318f61db3e2b5ae82
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-11 23:17:26 +0300

    349

--- test349/test349/Program.cs
+++ test349/test349/Program.cs
@@ -6,8 +6,15 @@ namespace test349
     {
         public static void Main(string[] args)
         {
-            int x = Convert.ToInt32(Console.ReadLine());
-            
+            string abcd = Console.ReadLine();
+            string dabc = abcd[3] + abcd.Substring(0, 3);
+            string cdab = dabc[3] + dabc.Substring(0, 3);
+            string bcda = cdab[3] + cdab.Substring(0, 3);
+            int max1 = Math.Max(Convert.ToInt32(abcd), Convert.ToInt32(dabc));
+            int max2 = Math.Max(Convert.ToInt32(cdab), Convert.ToInt32(bcda));
+            int max = Math.Max(max1, max2);
+            Console.WriteLine(max);
         }
     }
 }
+
