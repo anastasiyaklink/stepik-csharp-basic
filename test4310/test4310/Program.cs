547b5f24f4209ae7854c75d3733ee17c708b9d55
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-12 00:07:55 +0300

    test4310 конь

---
+++ test4310/test4310/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace test4310
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            if ((x2 == x1 + 1 && y2 == y1 + 2) ||
+                (x2 == x1 + 1 && y2 == y1 - 2) ||
+                (x2 == x1 - 1 && y2 == y1 + 2) ||
+                (x2 == x1 - 1 && y2 == y1 - 2) ||
+                (x2 == x1 + 2 && y2 == y1 + 1) ||
+                (x2 == x1 + 2 && y2 == y1 - 1) ||
+                (x2 == x1 - 2 && y2 == y1 + 1) ||
+                (x2 == x1 - 2 && y2 == y1 - 1))
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
