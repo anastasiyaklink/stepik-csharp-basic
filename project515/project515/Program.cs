c12648388d8b71bb46dc03aaa5b7ac65646ef1c9
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-13 02:16:12 +0300

    515 For квадраты чисел

---
+++ project515/project515/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace project515
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                if (Math.Pow(i, 2) <= (Math.Pow(N,2)))
+                {
+                    Console.WriteLine(Math.Pow(i, 2));
+                }
+            }
+        }
+    }
+}
