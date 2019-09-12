9bd1f6b2c7e968db7e84007089eb0da25cacefd9
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-13 01:55:22 +0300

    514 For Обратная последовательность

---
+++ project514/project514/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace project514
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = b; i >= a; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
