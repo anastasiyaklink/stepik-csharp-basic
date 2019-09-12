aace6a3bca141fdbf54269ca94e12ad11da242ee
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-13 01:36:23 +0300

    513 For делители числа

---
+++ project513/project513/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace project513
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= x; i++)
+            {
+                if (x % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
