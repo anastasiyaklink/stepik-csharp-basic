8d02c2588dd611a3e311d853b01188fc4d194f4f
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-13 00:58:30 +0300

    511 цикл For

---
+++ project511/project511/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace project511
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 19; i++)
+            {
+                Console.WriteLine("Hello, BeeGeek!");
+            }     
+        }
+    }
+}
