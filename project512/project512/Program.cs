72c7d05956a4ecab471d12c8e28f66fa33a5efd6
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-13 01:25:08 +0300

    512 for четные числа

---
+++ project512/project512/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace project512
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = a; i <= b; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
