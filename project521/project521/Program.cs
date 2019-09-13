19e3fca679634b464b2e2b351403a8a75b68f208
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-14 02:01:20 +0300

    521 For sum делители числа

---
+++ project521/project521/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace project521
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            int del = 0;
+            for (int i = 1; i <= x; i++)
+            {
+                if (x % i == 0)
+                {
+                    del = del + 1;
+                }
+
+            }
+            Console.WriteLine(del);
+        }
+    }
+}
