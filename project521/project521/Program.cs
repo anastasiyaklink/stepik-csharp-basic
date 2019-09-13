5b2fbe3c0cdbe7f91d804a59d1c6a81126c7fa1a
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-14 02:06:43 +0300

    Update Program.cs

--- project521/project521/Program.cs
+++ project521/project521/Program.cs
@@ -7,16 +7,16 @@ namespace project521
         public static void Main(string[] args)
         {
             int x = Convert.ToInt32(Console.ReadLine());
-            int del = 0;
+            int sum = 0;
             for (int i = 1; i <= x; i++)
             {
                 if (x % i == 0)
                 {
-                    del = del + 1;
+                    sum++;
                 }
 
             }
-            Console.WriteLine(del);
+            Console.WriteLine(sum);
         }
     }
 }
