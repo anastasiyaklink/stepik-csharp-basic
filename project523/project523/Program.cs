96577150b68e2cb367e99208702c1de164ae4a26
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-15 01:49:08 +0300

    523 For Sum сумма с условием

---
+++ project523/project523/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace project523
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            int num;
+
+            for (int i = 1; i <= N; i++)
+            {
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num % 2==0 && num % 3 !=0)
+                {
+                    sum = sum + num;
+                }
+                  
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
