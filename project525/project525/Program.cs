c12aa1e8e0a70b44ac22f36e6802a0444088122b
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-15 02:12:29 +0300

    525 for sum считает введенные нули

---
+++ project525/project525/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace project525
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            int num;
+
+            for (int i = 1; i <= N; i++)
+            {
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num == 0)
+                {
+                    sum = sum + 1;
+
+                    //Console.WriteLine("YES";
+                   // Console.WriteLine("NO");
+                }
+            }
+            if (sum>0)
+            {
+                Console.WriteLine("YES");
+            }
+            else Console.WriteLine("NO");
+        } 
+    }
+}
