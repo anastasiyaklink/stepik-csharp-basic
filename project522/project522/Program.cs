5377c3b8eb113283eb16bf4eb8089dd649c50047
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-15 01:40:28 +0300

    522 for sum сумматор

---
+++ project522/project522/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace project522
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            int number;
+
+            for (int i = 1; i <= N; i++)
+            {
+                number = Convert.ToInt32(Console.ReadLine());
+                sum = sum + number;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
