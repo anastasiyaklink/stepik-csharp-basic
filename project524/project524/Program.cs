c97d9fbc2ec5ecf90eb46c5017432612d1aa6139
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-15 01:56:40 +0300

    524 For sum кол-во чисел, оканчивающихся на 0

---
+++ project524/project524/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace project524
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            int num;
+
+            for (int i = 1; i <= N; i++)
+            {
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num % 10 == 0)
+                {
+                    sum = sum + 1;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
+   
