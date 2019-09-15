f12dafc798bb9cf81de024d2a8b7f01a08692086
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-15 12:41:03 +0300

    531 for факториал

---
+++ project531/project531/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace project531
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int fact = 1;
+
+            for (int i = 1; i <= N; i++)
+            {
+                fact = fact * i;
+            }
+            Console.Write(fact);
+        }
+    }
+}
