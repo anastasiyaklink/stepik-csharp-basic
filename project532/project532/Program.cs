0ef1b0f1f76966576020c89ad674b722b5d25730
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-16 20:08:01 +0300

    532 for mult произведение на отрезке

---
+++ project532/project532/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace project532
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int mult = 1;
+
+            for (int i = a; i <= b; i++)
+            {
+                mult = mult * i;
+            }
+            Console.Write(mult);
+        }
+    }
+}
