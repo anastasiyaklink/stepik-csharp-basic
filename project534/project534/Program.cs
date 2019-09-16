5d3a64dc84be470e0ecd514b603e05f1efd57923
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-17 01:28:00 +0300

    534 for mult произведение четных чисел от 1 до n

---
+++ project534/project534/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace project534
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int mult = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    mult = mult * i;
+                }
+            }
+            Console.WriteLine(mult);
+        } 
+    }
+}
