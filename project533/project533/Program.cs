ae5b2dcaedcf27ff2a74a2ef5e6f92f55b496f51
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-16 20:35:54 +0300

    533 for mult Произведение чисел, оканчивающихся на 7

---
+++ project533/project533/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace project533
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int mult = 1;
+
+            for (int i = a; i <= b; i++)
+            {
+                if (i % 10 == 7)
+                {
+                    mult = mult * i;
+                }
+            }
+            Console.Write(mult);
+        }
+    }
+}
