6d3d117bbbacdde0eed4d05a0facbed3ac9f9af9
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-18 02:34:50 +0300

    543 while Двоичный логарифм

---
+++ project543/project543/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace project543
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int k = 1;
+            int i = 0;
+            while (k < N)
+            {
+                k = 2 * k;
+                i++;
+            }
+            Console.WriteLine(i);
+        }
+    }
+}
