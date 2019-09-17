57f5abd8cb90f7fbde306bc1bfc2503be6845579
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-17 20:46:15 +0300

    541 While деление нацело на 3

---
+++ project541/project541/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace project541
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            while (a % 3 == 0)
+            {
+                a = a / 3;
+                i++;
+            }
+            Console.WriteLine(i);
+        }
+    }
+}
