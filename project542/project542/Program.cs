a95290416ce7310d1ba69a6a4266788e5e20fe0c
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-17 22:56:39 +0300

    542 while список степеней двойки

---
+++ project542/project542/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace project542
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int x = 1;
+            int i = 0;
+            while (x <= N)
+            {
+                Console.WriteLine(x);
+                x = x * 2;
+                i++;
+            }
+            
+        }
+    }
+}
