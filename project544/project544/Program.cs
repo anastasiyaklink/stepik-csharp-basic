c95eb97063f225c4139ecf9101140eb72b369175
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-18 21:15:38 +0300

    544 while алгоритм Евклида НОД

---
+++ project544/project544/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace project544
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            while ((a != 0) && (b != 0))
+            {
+                if (a > b)
+                    a = a - b;
+                else
+                    b = b - a;
+            }
+
+            Console.WriteLine(Math.Max(a, b));
+        }
+    }
+}
+
+
