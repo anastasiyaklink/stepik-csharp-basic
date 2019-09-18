178d83d7b4c72e64b2b43367644dd5a61feddfa6
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-18 22:12:45 +0300

    551 while кол-во четверок в числе

---
+++ project551/project551/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace project551
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            while (N > 0)
+            {
+                int digit = N % 10;
+
+                if (digit == 4)
+                {
+                    i = i + 1;
+                }
+                N = N / 10;
+            }
+            Console.WriteLine(i);
+        }
+    }
+}
