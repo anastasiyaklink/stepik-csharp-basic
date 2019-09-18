7646c55636bcda283d83dc404469b857f0f9681b
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-18 23:33:42 +0300

    553 while обратное число

---
+++ project553/project553/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace project553
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            while (N > 0)
+            {
+                int num = N % 10;
+                Console.Write(num);
+                N = N / 10;
+            }
+        }
+    }
+}
