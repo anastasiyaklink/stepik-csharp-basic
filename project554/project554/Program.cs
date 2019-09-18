b8175ade479140b2e503ae0c63f59b1baf393493
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-19 00:46:24 +0300

    554 while перевод числа в обратное двоичное

---
+++ project554/project554/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace project554
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            while (N > 0)
+            {
+                Console.Write(N % 2);
+                 N = N / 2;
+            }
+        }
+    }
+}
