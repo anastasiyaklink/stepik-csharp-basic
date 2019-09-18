2a8de0da6f3c211155ccbea3fdfade64f0f713e1
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-19 01:41:10 +0300

    563 while Средне арифметическое последовательности

---
+++ project563/project563/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace project563
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            double sum = 0;
+            int i = 0;
+            while (N != 0)
+            {
+                sum = sum + N;
+                i = i + 1;
+                N = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine(sum/i);
+        }
+    }
+}
