35f46fda96e96f38b2f8f6d27fcaefa7be8ab2e4
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-19 01:13:30 +0300

    561 while пока не введешь 0 считать ( четные и не кратные трем) сумму

---
+++ project561/project561/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace project561
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            while (N != 0)
+            {
+               if ( N%2==0 && N % 3 != 0)
+                {
+                    sum = sum + N;
+                }
+               N = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
