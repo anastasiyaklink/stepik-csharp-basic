6b8810143349ac527a3db51bcfe11ddf955194bd
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-19 01:27:45 +0300

    562 while Разность положительных и отрицательных чисел

---
+++ project562/project562/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace project562
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+
+            while (N != 0)
+            {
+                if (N > 0)
+                {
+                    sum = sum + 1;
+                }
+                else
+                {
+                    sum = sum - 1;
+                }
+
+                N = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
