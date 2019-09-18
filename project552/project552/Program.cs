cc1699c3ac0afe9c0930e61dcb06fe65dff4e885
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-18 23:18:14 +0300

    552 while Замечательное число

---
+++ project552/project552/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace project552
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int N1 = N;
+            int sum = 0;
+
+            while (N1 > 0)
+            {
+                int digit = N1 % 10;
+                sum = sum + digit; // sum = sum+digit
+                N1 = N1 / 10; //N1=N1/10
+            }
+            Console.WriteLine(sum);
+            Console.WriteLine((N % sum == 0) ? "YES" : "NO");
+        }
+    }
+}
