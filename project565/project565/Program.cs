9e442efffd7de0d860f2981b1163ecfd3797c3b7
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-19 17:09:07 +0300

    565 while последовательность, счетчик изменения знака числа -+

---
+++ project565/project565/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace project565
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            int N1 = N;
+            while (N != 0)
+            {
+                if ((N > 0 && N1 < 0) || (N < 0 && N1 > 0))
+                {
+                    sum = sum + 1;
+                }
+                N1 = N;
+                N = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
