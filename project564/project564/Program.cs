ae0f70bae0261503f8d5a930fd4ed345b8b1e040
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-19 16:47:29 +0300

    564 while последовательность,  кол-во элементов, больше предыдущего

---
+++ project564/project564/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace project564
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            int N1 = N;
+            while (N != 0)
+            {
+                if (N>N1)
+                {
+                    sum = sum + 1;
+                }
+                N1 = N;
+                N = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
