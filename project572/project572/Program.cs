6fc61ca379f77d882d95bb184321ce68a579f955
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-21 02:58:45 +0300

    572 вложенные циклы Количество цифр 7

---
+++ project572/project572/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace project572
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 1; i <= N; i++)
+            {
+                int x = i;
+
+                while (x > 0)
+                {
+                    int y = x % 10;
+
+                    if (y == 7) sum = sum + 1;
+                    x = x / 10;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
+
+
+
+
+
+
