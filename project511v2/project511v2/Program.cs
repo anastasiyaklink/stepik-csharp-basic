fedd2d6042c53c31d28c7d10b0b60f48c28994f1
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-20 23:43:49 +0300

    555 -2 while перевод числа в двоичную

---
+++ project511v2/project511v2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace project511v2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int k = 0;
+            int km = 1;
+
+            //  Find k = max number of significant bits
+            while (km < n)
+            {
+                km *= 2;
+                k++;
+            }
+
+            int nk = n;
+
+            // Skip highest 0, unless it's the only 0
+            if (nk != 0 && nk < km)
+            {
+                km /= 2;
+                k--;
+            }
+
+            while (k >= 0)
+            {
+                if (nk >= km)
+                {
+                    Console.Write(1);
+                    nk -= km;
+                }
+                else
+                {
+                    Console.Write(0);
+                }
+
+                km /= 2;
+                k--;
+            }
+        }
+    }
+}
