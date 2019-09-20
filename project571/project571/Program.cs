a725d80e704b85d714654fb24323423f87b8ae67
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-21 01:09:12 +0300

    571 вложенные циклы Делители на отрезке

---
+++ project571/project571/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace project571
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = a; i <= b; i++)
+            {
+                int sum = 0;
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0) sum = sum + 1;
+                }
+
+                if (sum >= k) Console.Write(i + " ");
+            }
+        }
+    }
+}
+
