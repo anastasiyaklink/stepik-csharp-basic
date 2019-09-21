72f1bdd31c62e180a3cded1a3d0dfba08b5a4d94
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-22 01:08:18 +0300

    573 вложенные циклы Максимальная сумма цифр

---
+++ project573/project573/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace project573
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int x = 0;
+            int y = 0;
+            int j = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                int sum = 0;
+                
+                for (j = i; j > 0; j = j / 10)
+                {
+                    sum = sum + (j % 10);
+                }
+
+                if (y < sum)
+                {
+                    y = sum;
+
+                    x = i;
+                }
+            }
+            Console.WriteLine(x + " " + y);
+        }
+    }
+}
+
+
+
