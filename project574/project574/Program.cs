a903d3dd3747ddb70ee14dc96ce5920aaabc78c8
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-23 01:05:57 +0300

    574 Вложенные циклы Разложение на простые множители

---
+++ project574/project574/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace project574
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int divider = 2;
+            int dividend = N;
+
+            while (dividend !=1)
+            {
+                if (dividend % divider == 0)
+                {
+                    Console.Write(divider + " ");
+                    dividend = dividend / divider;
+                }
+                else divider++;
+            }
+        }
+    }
+}
