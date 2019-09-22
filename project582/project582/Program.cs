66dd7dfbbc04192aaaa9b9f4de19ddedebf33570
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-23 00:39:17 +0300

    582 break и continue числа от 1 до 100

---
+++ project582/project582/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace project582
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = 0;
+            while (N <= 100)
+            {
+                N = Convert.ToInt32(Console.ReadLine());
+                if (N >= 10 && N <= 100) Console.WriteLine(N);
+
+                if (N < 10) continue;
+
+                if (N > 100) break;
+            }
+        }
+    }
+}
