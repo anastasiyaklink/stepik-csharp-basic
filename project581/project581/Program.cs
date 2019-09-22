cdf3bcc71b8438460132221798980f3a6442e055
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-22 23:55:26 +0300

    581 Break и continue Максимальный делитель

---
+++ project581/project581/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace project581
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            for (int i = N-1; i>=1; i--)
+            {
+                if (N%i==0)
+                {
+                    Console.WriteLine(i);
+                    break; 
+                }
+            }
+        }
+    }
+}
