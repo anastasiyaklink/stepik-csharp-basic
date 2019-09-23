f1a943a216dda7ed4532c2dc19fefe5d1a160dac
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-23 23:48:16 +0300

    583 break и continue Первое чуть кратное

---
+++ project583/project583/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace project583
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int d = Convert.ToInt32(Console.ReadLine());
+            int x = 1;
+            while (x <= n)
+            {
+               int divider = n / x;
+                x++;
+                if (divider % c == 0 && divider % d != 0)
+                {
+                    Console.WriteLine(divider);
+                    break;
+                }
+            }
+        }
+    }
+}
