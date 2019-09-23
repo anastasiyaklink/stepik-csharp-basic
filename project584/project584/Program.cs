2e519eab3fc3e5a3b84627fcb7a00e6815352848
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-24 00:35:01 +0300

    584 break и continue Номер строки
    По данной последовательности строк, определите какой по счету встретится строка 𝑠.
    
    Входные данные
    Вводится строка 𝑠, а далее последовательность строк. Каждая строка по длине не превышает 30 символов.
    
    Выходные данные
    Определите каким по счету встретится строка 𝑠 в данной последовательности и выведите этот номер.
    
    Примечание: гарантируется, что данная строка встретится в последовательности.

---
+++ project584/project584/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace project584
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int i = 1;
+
+            while (s != Console.ReadLine())
+            {
+                i++;
+            }
+
+            Console.WriteLine(i);
+        }
+    }
+}
