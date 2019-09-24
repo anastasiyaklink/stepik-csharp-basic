39ae721e40f2f042d61129932d72a01cf22995df
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-24 22:02:15 +0300

    591 перебор Кубическое уравнение
    По данным числа 𝑎,𝑏,𝑐 и 𝑑 найдите  все целые решения уравнения 𝑎𝑥3+𝑏𝑥2+𝑐𝑥+𝑑=0 на отрезке [0,1000] и выведите их в порядке возрастания.
    Входные данные
    Вводятся 4 целых числа: 𝑎,𝑏,𝑐 и 𝑑, каждое из которых не превосходит 30000
    Выходные данные
    Выведите на одной строке через пробел все целые решения уравнения на отрезке [0,1000] и выведите их в порядке возрастания. Если на данном отрезке нет ни одного решения, то ничего выводить не нужно.

---
+++ project591/project591/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace project591
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int d = Convert.ToInt32(Console.ReadLine());
+            
+            for (int x = 0; x <= 1000; x++)
+            {
+                if (a * x * x * x + b * x * x + c * x + d == 0)
+                {
+                    Console.Write(x+ " ");
+                }
+            }
+        }
+    }
+}
