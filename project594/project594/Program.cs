51ca8c02d11fdd0a928dc2e15726845e9481850c
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-26 01:03:45 +0300

    594 перебор удвоенная сумма цифр = числу
    Напишите программу, которая выводит все двузначные числа, которые равны удвоенному произведению своих цифр.
    В ответе запишите найденные числа через запятую без пробелов.

---
+++ project594/project594/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace project594
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int x = 10;
+            int i = x;
+            while (i <= 99)
+            {
+                if (i / 10 * (i % 10) * 2 == i) Console.WriteLine(i + ",");
+                i++;
+            }
+        }
+    }
+}
+
