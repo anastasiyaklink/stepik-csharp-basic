06b10f673c39caf2c2dda259e00e91580d3ca7cc
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-25 23:14:39 +0300

    592 перебор Кратные
    Найдите все числа от 1 до 𝑛 кратные 𝑐, но НЕ кратные 𝑑.
    
    Входные данные
    Вводится 3 натуральных числа 𝑛,𝑐,𝑑, каждое из которых не превышает 10000
    .
    Выходные данные
    Вывести все числа от 1 до 𝑛 включительно, кратные 𝑐, но НЕ кратные 𝑑.

---
+++ project592/project592/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace project592
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int d = Convert.ToInt32(Console.ReadLine());
+
+            for (int x = 1; x <= n; x++)
+            {
+
+                if (x % c == 0 && x % d != 0)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+        }
+    }
+}
