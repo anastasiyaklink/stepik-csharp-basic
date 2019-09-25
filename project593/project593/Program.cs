1145396e6bcd51ea9c7573ce35c43776d07775c2
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-26 00:14:41 +0300

    593 перебор Анализ трехзначных чисел
    Напишите программу, которая в последовательности трехзначных чисел находит количество всех чисел сумма цифр которых равна 8.
    В ответе запишите найденное количество чисел.

---
+++ project593/project593/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace project593
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int x = 100;
+            int i = x;
+            int sum = 0;
+            while (i <= 999)
+            {
+                if ((i / 100) + (i / 10 % 10) + (i % 10) == 8)
+                {
+                    sum = sum + 1;
+                }
+                i++;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
