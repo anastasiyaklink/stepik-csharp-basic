763c231aa6a75773dcb5677bc58d4678366d6a64
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-27 00:36:08 +0300

    595 перебор Сложное кубическое уравнение
    По данным целым числам 𝑎,𝑏,𝑐,𝑑, найдите количество целых решений уравнения (𝑎𝑥3+𝑏𝑥2+𝑐𝑥+𝑑)/(𝑥−𝑒)=0 на отрезке [0,1000].
    Входные данные
    Вводятся 5 целых чисел: 𝑎,𝑏,𝑐,𝑑,e каждое из которых по модулю не превышает 10000.
    Выходные данные
    Выведите количество целых решений заданного уравнения на отрезке [0,1000]

---
+++ project595/project595/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace project595
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int d = Convert.ToInt32(Console.ReadLine());
+            int e = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            for (int x = 0; x <= 1000; x++)
+            {
+                if (x == 0 && e == 0 || x == e)
+                    continue;
+                else if (a * x * x * x + b * x * x + c * x + d / (x - e) == 0)
+                    i++;
+            }
+            Console.Write(i);
+        }
+    }
+}
+
