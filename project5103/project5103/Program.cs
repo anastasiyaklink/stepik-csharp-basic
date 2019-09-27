5de9af01985fa8638cf430e1263f47243f81a329
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-27 23:44:10 +0300

    5103  Минимаксные задачи Разница между экстремумами
    Найдите разницу между максимальным и минимальным числами.
    Входные данные
    Вводится натуральное число 𝑁(𝑁≤105), а затем 𝑁 чисел, каждое из которых по модулю не превышает 10^6
    Выходные данные
    Выведите разницу между максимальным и минимальным числами.

---
+++ project5103/project5103/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace project5103
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int num;
+            int max = -(int)Math.Pow(10, 6);
+            int min = (int)Math.Pow(10, 6);
+            for (int i = 1; i <= N; i++)
+            {
+                num = Convert.ToInt32(Console.ReadLine());
+
+                if (num > max)
+                {
+                    max = num;
+                }
+                if (num < min)
+                {
+                    min = num;
+                }
+            }
+            Console.WriteLine(max - min);
+        }
+    }
+}
