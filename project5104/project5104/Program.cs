01a7d0dab2f105a7481a8a31182196d7994e872f
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-28 00:08:18 +0300

    5104 Минимаксные задачи Камера наблюдения
    Камера наблюдения регистрирует в автоматическом режиме скорость проезжающих мимо неё автомобилей. Необходимо определить максимальную зарегистрированную скорость автомобиля. Если скорость хотя бы одного автомобиля была меньше 30 км/ч, выведите «YES», иначе выведите «NO».
    Входные данные
    Программа получает на вход число проехавших автомобилей 𝑁(1<𝑁<30)
    , затем указываются их скорости. Значение скорости не может быть меньше 1
    1 и больше 300
    Выходные данные
    Выведите максимальную зарегистрированную скорость автомобиля. А затем, если скорость хотя бы одного автомобиля была меньше 30 км/ч, выведите «YES» в той же строке, иначе выведите «NO».

---
+++ project5104/project5104/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace project5104
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int speed;
+            int max = 0;
+            int min = 30;
+            bool found = false;
+
+            for (int i = 1; i <= N; i++)
+            {
+                speed = Convert.ToInt32(Console.ReadLine());
+
+                if (speed > max)
+                {
+                    max = speed;
+                }
+                if (speed < min)
+                {
+                    found = true;
+                }
+            }
+            Console.Write(max + " " + (found ? "YES" : "NO"));
+        }
+    }
+}
