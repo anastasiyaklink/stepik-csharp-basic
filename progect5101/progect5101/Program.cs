f70c98705c17d19281632e398fd34d92301dafc5
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-27 23:09:32 +0300

    5101 Минимаксные задачи Минимум
    Найдите минимальное среди последовательности целых чисел.
    Входные данные
    Вводится натуральное число 𝑁(𝑁≤105), а затем 𝑁 чисел, каждое из которых по модулю не превышает 106
    Выходные данные
    Выведите минимальное из введенных чисел .

---
+++ progect5101/progect5101/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace progect5101
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int num;
+            int min = (int)Math.Pow(10, 6);
+            for (int i = 1; i <= N; i++)
+            {
+                num = Convert.ToInt32(Console.ReadLine());
+
+                if (num < min)
+                {
+                    min = num;
+                }
+            }
+            Console.WriteLine(min);
+        }
+    }
+}
