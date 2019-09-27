c2f3c4d6eec3684c8b8df9df5c829c53413abc6a
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-27 23:29:32 +0300

    5102 Минимаксные задачи Максимум
    Найдите максимальное из введенных чисел. Входные данные
    Вводится натуральное число 𝑁(𝑁≤105), а затем 𝑁
    целых чисел, каждое из которых по модулю не превышает 10^6
    Выходные данные
    Выведите максимальное из введенных чисел.

---
+++ project5102/project5102/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace project5102
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int num;
+            int max = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                num = Convert.ToInt32(Console.ReadLine());
+
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
