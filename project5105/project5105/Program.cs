0c05c1a4bdcb4da7454345352bbf497493ad943e
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-29 01:37:26 +0300

    5105 Минимаксные задачи Второй максимум
    Последовательность состоит из различных натуральных чисел и завершается числом 0. Определите значение второго по величине элемента в этой последовательности. Числа, следующие за числом 0 считывать не нужно.
    Входные данные
    Вводится последовательность натуральных чисел, оканчивающаяся числом 0(само число 0 в последовательность не входит). Числа не превышают 1000. Гарантируется, что последовательность состоит как минимум из двух чисел.
    Выходные данные
    Выведите значение второго по величине элемента.

---
+++ project5105/project5105/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace project5105
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int max = 0;
+            int max2 = 0;
+            while (N != 0)
+            {
+                if (N > max)
+                {
+                    max2 = max;
+                    max = N;
+                }
+                else if (N > max2)
+                {
+                    max2 = N;
+                }
+
+                N = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine(max2);
+        }
+    }
+}
