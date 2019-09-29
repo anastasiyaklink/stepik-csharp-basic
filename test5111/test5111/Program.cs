d2bf2bb354fd8fdf57923445133c38885847a830
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-30 00:00:11 +0300

    5111 Итоговый тест Количество нулей
    По данным числам, определите количество чисел, которые равны нулю.
    Входные данные
    Вводится натуральное число 𝑁(𝑁≤105), а затем 𝑁 чисел, каждое из которых не превышает 2∗104 Выходные данные
    Выведите количество чисел, которые равны нулю.

---
+++ test5111/test5111/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace test5111
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                if (Convert.ToInt32(Console.ReadLine()) == 0)
+                {
+                    sum++;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
