3dd2fa008179eadbf28c84539f99081a8ba10d6a
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-30 00:20:29 +0300

    5112 Итоговый тест Палиндром?
    По данному числу определите, является ли оно палиндромом?
    Входные данные
    Вводится одно натуральное число 𝑛, не превышающее 10^9
    Выходные данные
    Вывести "YES", если число является палиндромом , в противном случае - "NO".
    Примечание: гарантируется, что число не оканчивается на ноль.

---
+++ test5112/test5112/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace test5112
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            char[] array = n.ToCharArray();
+            Array.Reverse(array);
+            string n2 = new string(array);
+            Console.WriteLine(n == n2 ? "YES" : "NO");
+        }
+    }
+}
