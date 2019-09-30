b389264e962d76433708c94d97d8bc106cd278ea
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-10-01 00:19:43 +0300

    5115 Итоговый тест Количество минимумов
    Найдите количество минимальных элементов в последовательности.
    Входные данные
    Вводится натуральное число 𝑁(𝑁≤105), а затем 𝑁 чисел. Все числа по модулю не превышают 10^6
    Выходные данные
    Выведите количество минимальных элементов.

---
+++ test5115/test5115/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace test5115
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            double min = Math.Pow(10, 6);
+            int sum = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                int num = Convert.ToInt32(Console.ReadLine());
+                if (min > num)
+                {
+                    min = num;
+                    sum = 1;
+                }
+                else if (num == min)
+                {
+                    sum++;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
