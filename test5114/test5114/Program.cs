3bfaee473d1ebdbc81022f6a570916707e9fa933
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-30 23:15:33 +0300

    5114 Итоговый тест Степень числа
    Для целых чисел 𝑎 и 𝑛
    вычислить 𝑎^n
    Входные данные
    Вводятся два целых числа - 𝑎 и 𝑛 , при этом 1≤𝑎<100,0≤𝑛≤20.
    Выходные данные
    Выводится одно число - 𝑎𝑛.
    Примечание: гарантируется, что ответ не больше 2⋅10^9. НЕ используйте Math.Pow!

---
+++ test5114/test5114/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace test5114
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int n = Convert.ToInt32(Console.ReadLine());
+            int an = a;
+            if (n == 0)
+            {
+                an = 1;
+            }
+            else
+            {
+                for (int i = 1; i < n; i++)
+                {
+                    an = an * a;
+                }
+            }
+            Console.WriteLine(an);
+        }
+    }
+}
