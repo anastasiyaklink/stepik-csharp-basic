849eb28e47c587e7ed5b123cd25885b3e462d7f2
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-09-30 23:29:26 +0300

    5113 Итоговый тест 5 и 7
    Из данного числа выбросите цифры 5 и 7, при этом порядок остальных цифр не меняется.
    Входные данные
    Вводится одно натуральное число 𝑛, не превышающее 10000
    Выходные данные
    Вывести число 𝑛, без цифр 5 и 7
    Примечание: гарантируется, что число содержит хотя бы 1
    символ, отличный от 5 и 7

---
+++ test5113/test5113/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace test5113
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            Console.WriteLine(s.Replace("5", "").Replace("7", ""));
+        }
+    }
+}
