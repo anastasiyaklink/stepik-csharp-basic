16a1dbbaf71d79cb1541e93ce0947bd2be80b8d7
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-10-01 00:40:30 +0300

    5117 Итоговый тест Самое большое число на отрезке, кратное 7
    Найдите самое большее число на отрезке от 𝑎 до 𝑏, кратное 7
    Входные данные
    Вводится два целых числа 𝑎 и 𝑏(𝑎≤𝑏) , каждое из которых по модулю не превышает 2∗10^4
    Выходные данные
    Найдите самое большее число на отрезке от 𝑎 до 𝑏, кратное 7, или выведите "NO" - если таковых нет.

---
+++ test5117/test5117/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace test5117
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            for (int i = b; i >= a; i--)
+            {
+                if (i % 7 == 0)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+                else if (i == a)
+                {
+                    Console.WriteLine("NO");
+
+                }
+            }
+        }
+    }
+}
