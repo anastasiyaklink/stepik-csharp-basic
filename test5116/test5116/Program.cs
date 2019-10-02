1895265a7cb4ca72522be002c4acea05663d1db5
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-10-03 01:02:09 +0300

    5116 Итоговый тест Цифровой корень
    Цифровой корень натурального числа — это цифра, полученная в результате итеративного процесса суммирования цифр, на каждой итерации которого для подсчета суммы цифр берут результат, полученный на предыдущей итерации. Этот процесс повторяется до тех пор, пока не будет получена одна цифра.
    Например цифровой корень 65536 это 7, потому что 6+5+5+3+6=25 и 2+5=7
    По данному числу определите его цифровой корень.
    Входные данные
    Вводится одно натуральное число 𝑛, не превышающее 10^7
    Выходные данные
    Вывести цифровой корень числа 𝑛

---
+++ test5116/test5116/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace test5116
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+          
+            int N = Convert.ToInt32(Console.ReadLine());
+            int N1 = N;
+            int sum = 0;
+           
+            
+            if (N1>=10)
+            {
+                while (N1 > 0)
+                {
+                    int digit = N1 % 10;
+                    sum = sum + digit;
+                    N1 = N1 / 10;
+                }
+                while (sum >=10)
+                {
+                    N1 = sum;
+                    sum = 0;
+                    while (N1 > 0)
+                    {
+                        int digit = N1 % 10;
+                        sum = sum + digit;
+                        N1 = N1 / 10;
+                    }
+                }
+            }
+
+            else if (N1 < 10)
+            {
+                sum = N1;
+            }
+
+            Console.WriteLine(sum);   
+        }
+    }
+
+}
+    
