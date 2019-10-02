7133602c730fa4ffcf309fdf86ff9a16974304d2
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-10-03 01:13:17 +0300

    Update Program.cs

--- test5116/test5116/Program.cs
+++ test5116/test5116/Program.cs
@@ -6,39 +6,11 @@ namespace test5116
     {
         public static void Main(string[] args)
         {
-          
-            int N = Convert.ToInt32(Console.ReadLine());
-            int N1 = N;
-            int sum = 0;
-           
-            
-            if (N1>=10)
-            {
-                while (N1 > 0)
-                {
-                    int digit = N1 % 10;
-                    sum = sum + digit;
-                    N1 = N1 / 10;
-                }
-                while (sum >=10)
-                {
-                    N1 = sum;
-                    sum = 0;
-                    while (N1 > 0)
-                    {
-                        int digit = N1 % 10;
-                        sum = sum + digit;
-                        N1 = N1 / 10;
-                    }
-                }
-            }
 
-            else if (N1 < 10)
-            {
-                sum = N1;
-            }
+            int n = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine(sum);   
+            int m = (n - 1) % 9 + 1;
+            Console.Write(m);
         }
     }
 
