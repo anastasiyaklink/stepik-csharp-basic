7347b27e812ac3d617aa17a281bf3da3671d2397
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-10-04 01:40:36 +0300

    613 Символы Часть алфавита-2
    По заданной строчной букве латинского алфавита, выведите все строчные буквы латинского алфавита, начиная от заданной буквы до конца алфавита.
    Входные данные
    Задан единственный символ 𝑐 - строчная буква латинского алфавита.
    Выходные данные
    Выведите алфавит латинского алфавита от символа 𝑐 до конца алфавита на одной строке через пробел.

---
+++ project613/project613/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace project613
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            char c = Convert.ToChar(Console.ReadLine());
+            for (char w = c; w <= 'z'; w++)
+            {
+                Console.Write(w + " ");
+            }
+        }
+    }
+}
