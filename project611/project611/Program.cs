be26308130e7227f7e9559b99b8b324e3a1cce3f
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-10-03 23:01:42 +0300

    Update Program.cs

--- project611/project611/Program.cs
+++ project611/project611/Program.cs
@@ -7,14 +7,8 @@ namespace project611
         public static void Main(string[] args)
         {
             char c = Convert.ToChar(Console.ReadLine());
-            if (c >= '0' && c <= '0')
-            {
-                Console.WriteLine("YES");
-            }
-            else
-            {
-                Console.WriteLine("NO");
-            }
+            Console.WriteLine(c >= '0' && c <= '9' ? "YES" : "NO");
+        
         }
     }
 }
