d69c61ab134a7d9897c49e276a23ac8bc50f132b
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-10-03 23:00:14 +0300

    611 Символы Цифра
    Определите является ли данный символ цифрой или нет.
    Входные данные
    Задан единственный символ 𝑐
    Выходные данные
    Необходимо вывести "YES", если символ является цифрой, и "NO" - в противном случае.

---
+++ project611/project611/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace project611
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            char c = Convert.ToChar(Console.ReadLine());
+            if (c >= '0' && c <= '0')
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
