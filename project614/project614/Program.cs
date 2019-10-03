e49fe25fa7b25782f34e3088fa72d708b801217c
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-10-04 02:18:21 +0300

    614 Символы Изменить регистр символа
    Измените регистр символа, если он был латинской буквой: сделайте его заглавным, если он был строчной буквой и наоборот.
    Входные данные
    Задан единственный символ латинского алфавита 𝑐.
    Выходные данные
    Необходимо вывести получившийся символ.

---
+++ project614/project614/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace project614
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            char c = Convert.ToChar(Console.ReadLine());
+            int code = c;
+            char d = c;
+            if (code >= 97 && code <= 122)
+            {
+                code = (char)(d - 32);
+            }
+
+            else if (code >= 65 && code <= 90)
+            {
+                code = (char)(d + 32);
+            }
+
+            Console.WriteLine((char)code);
+        }
+    }
+}
