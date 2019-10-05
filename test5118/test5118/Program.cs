190616c386452228bb08e940c53c1c841d585c2a
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-10-05 23:06:49 +0300

    5118 Итоговый тест Дружественные числа
    Два различных натуральных числа 𝑛 и 𝑚 называются дружественными, если сумма делителей числа 𝑛 (включая 1, но исключая само 𝑛) равна числу 𝑚 и наоборот. Например, 220 и 284 – дружественные числа. По данному числу 𝑘 выведите все пары дружественных чисел, каждое из которых не превосходит 𝑘
    Входные данные
    Программа получает на вход одно натуральное число 𝑘, не превосходящее 10^5.
    Выходные данные
    Программа должна вывести  все пары дружественных чисел, каждое из которых не превосходит 𝑘. Пары необходимо выводить по одной в строке, разделяя пробелами. Каждая пара должна быть выведена только один раз (перестановка чисел новую пару не дает). Все пары необходимо выводить следующий образом: сначала выводим меньше из двух чисел. Все пары должны быть отсортированы по меньшему числу в паре.

---
+++ test5118/test5118/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace test5118
+{
+    class MainClass
+    {
+        static int SumFactors(int N)
+        {
+            int sum = 1;
+
+            double root = Math.Sqrt(N);
+
+            for (int i = 2; i < root; i++)
+            {
+                if (N % i == 0)
+                {
+                    sum = sum + i + N / i;
+                }
+            }
+
+            return sum;
+        }
+
+        public static void Main(string[] args)
+        {
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            for (int n = 1; n < k; n++)
+            {
+                int m = SumFactors(n);
+
+                if (n < m && SumFactors(m) == n)
+                {
+                    Console.WriteLine(n + " " + m);
+                }
+            }
+        }
+    }
+}
