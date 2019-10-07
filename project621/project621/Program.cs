60b630ec3666dd608eeca27ee0dcc9d2c691645c
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-10-07 23:36:46 +0300

    Строки Количество слов
    Дана строка, содержащая пробелы. Найдите, сколько в ней слов.
    Слово – это последовательность не пробельных символов. Слова разделены одним пробелом, первый и последний символ строки – не пробел.
    Входные данные
    На вход подается непустая строка, длина которой не превышает 1000.
    Выходные данные
    Необходимо вывести количество слов в строке.

---
+++ project621/project621/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace project621
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int countChar = s.Length;
+
+            int countA = 1;
+            for (int index = 0; index <= countChar - 2; index++)
+            {
+                if (s[index] == ' ' && s[index+1] != ' ')
+                {
+                    countA++;
+                }
+            }
+
+            Console.WriteLine(countA);
+        }
+    }
+}
