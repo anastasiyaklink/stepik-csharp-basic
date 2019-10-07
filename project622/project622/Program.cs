1ab19936e1509fd59062d67faa81392272db9af4
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-10-07 23:55:05 +0300

    622 Строки Перевертыш
    Дана строка. Найдите перевернутую ей строку.
    Входные данные
    На вход подается строка, длина которой не превосходит 1000
    Выходные данные
    Необходимо вывести ее перевертыш.

---
+++ project622/project622/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace project622
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int countChar = s.Length;
+
+            for (int index = countChar - 1; index >= 0; index--)
+            {
+                Console.Write(s[index]);
+            }
+
+        }
+    }
+}
