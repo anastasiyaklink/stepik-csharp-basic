bafa9f97963141cd945fdcbf3a0dc6d3ddc8b898
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-10-08 00:21:56 +0300

    623 Строки k -ый символ
    Дана строка. Удалите 𝑘-ый символ в ней.
    Входные данные
    На первой строке вводится строка 𝑠. На второй строке вводится целое число 1≤𝑘≤|𝑠|, где |𝑠| длина строки 𝑠.
    Выходные данные
    Необходимо вывести строку 𝑠 без 𝑘-го символа.

---
+++ project623/project623/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace project623
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= s.Length - 1; i++)
+            {
+                if (i != k - 1)
+                {
+                    Console.Write(s[i]);
+                }
+            }
+        }
+    }
+}
