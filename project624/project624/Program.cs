13fcbffd7149b726e7454bf1be10a09639014da6
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-10-08 23:27:12 +0300

    624 Строки Какой символ встречается раньше
    Дана строка. Определите, какой символ в ней встречается раньше: 'x' или 'w'. Если таких символов нет, вывести "-1".
    Входные данные
    На вход подается одна строка.
    Выходные данные
    Необходимо вывести букву 'x' или 'w', которая встречается в строке раньше. Если таких символов нет в строке, то вывести "-1".

---
+++ project624/project624/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace project624
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            for (int index = 0; index <= s.Length - 1; index++)
+            {
+                if (s[index] == 'x')
+                {
+                    Console.WriteLine("x");
+                    break;
+                }
+
+                if (s[index] == 'w')
+                {
+                    Console.WriteLine("w");
+                    break;
+                }
+
+                if (index == s.Length - 1)
+                {
+                    Console.WriteLine("-1");
+                }
+            }
+        }
+    }
+}
