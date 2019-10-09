2dcdd808053f871f9407e2816119748e26dd36b7
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-10-10 02:19:10 +0300

    628 Строки Нормируй строку!
    Вводится ненормированная строка, у которой может быть более одного пробела между словами. Привести ее к нормированному виду, т.е. между словами оставить только один пробел.
    Входные данные
    На вход подается одна строка.
    Выходные данные
    Необходимо вывести строку в нормированном виде.

---
+++ project628/project628/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace project628
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            //int countChar = s.Length;
+
+            //int countA = 1;
+            for (int i = 0; i <= s.Length - 1; i++)
+            {
+                if (s[i] == ' ' && s[i + 1] == ' ')
+                {
+                    continue;
+                }
+
+                //if ((s[i] != ' ' && s[i + 1] != ' ')||
+                    //(s[i] == ' ' && s[i + 1] != ' ')||
+                    //(s[i] != ' ' && s[i + 1] == ' '))
+                //{
+                   // Console.Write(s[i]);
+                //}
+             Console.Write(s[i]);
+                 
+
+            }
+
+            ///Console.WriteLine(countA);
+        }
+    }
+}
