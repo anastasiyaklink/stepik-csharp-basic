348215be2db6d2a1a201131f6d2fdbe8f3571ac5
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-10-10 02:41:07 +0300

    625 Строки  Палиндром?
    По данной строке определите, является ли она палиндромом? То есть, которое одинаково читается слева направо и справа налево. Например, слово "шалаш".
    Входные данные
    На вход подается одна строка без пробелов.
    Выходные данные
    Необходимо вывести  "YES", если строка является палиндромом, и "NO" - в противном случае.

---
+++ project625/project625/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace project625
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            bool palindrome = true;
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                if (s[i] != s[s.Length - i - 1])
+                {
+                    palindrome = false;
+                    break;
+                }
+            }
+            Console.WriteLine(palindrome ? "YES" : "NO");
+        }
+    }
+}
