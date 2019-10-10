1e07e4f6fbfad9bcfd50c7d5e6197197eef8a53d
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-10-11 00:33:18 +0300

    626 Строки Две одинаковые буквы
    Дана строка. Известно, что она содержит ровно две одинаковые буквы. Найдите эти буквы. Гарантируется, что повторяются буквы только одного вида.
    Входные данные
    На вход подается одна строка. Строка может состоять как из строчных, так и заглавных букв.
    Выходные данные
    Необходимо вывести букву, которая встречается в строке дважды.

---
+++ project626/project626/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace project626
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int flag = 0;
+            for (int i = 0; i <= s.Length - 1; i++)
+            {
+                for (int j = 0; j <= s.Length - 1; j++)
+                {
+                    
+                    if (i!=j && s[i] == s[j])
+                    {
+                        flag++;
+
+                        if (flag == 1)
+                        {
+                            Console.Write(s[j]);
+                            break;
+                        }
+
+                    }
+
+                }
+                //if (flag == 1) break;
+            }
+        }
+    }
+}
