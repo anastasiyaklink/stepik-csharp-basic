6469a98d7c1121ac1551d811252c7bf7612708fb
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-10-11 23:38:14 +0300

    631 Итоговый тест  K-ый символ
    По данной строке, найдите ее 𝑘-ый символ.
    Входные данные
    На вход программе дается непустая строка 𝑠, длины не больше 1000.
    На следующей строке вводится натуральное число 𝑘, не превосходящее 1000
    Выходные данные Выведите 𝑘-ый символ строки, если он существует, в противном случае выведите "NO".

---
+++ test631/test631/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace test631
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int k = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(k < s.Length ? s[k - 1].ToString() : "NO");
+            //for (int i = 0; i <= s.Length - 1; i++)
+            //{
+            //    if (k > s.Length - 1)
+            //    {
+            //        Console.WriteLine("NO");
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(s[k - 1]);
+            //        break;
+            //    }
+            //}
+        }
+    }
+}
