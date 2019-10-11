06175d9a8a816f066ce69b35edfe9bfb2fadfb4f
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-10-11 23:40:49 +0300

    Update Program.cs

--- test631/test631/Program.cs
+++ test631/test631/Program.cs
@@ -8,20 +8,7 @@ namespace test631
         {
             string s = Console.ReadLine();
             int k = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(k < s.Length ? s[k - 1].ToString() : "NO");
-            //for (int i = 0; i <= s.Length - 1; i++)
-            //{
-            //    if (k > s.Length - 1)
-            //    {
-            //        Console.WriteLine("NO");
-            //        break;
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine(s[k - 1]);
-            //        break;
-            //    }
-            //}
+            Console.WriteLine(k - 1 < s.Length ? s[k - 1].ToString() : "NO");
         }
     }
 }
