9b701f4a6e3eb9087c6f131ddc26dd7100e12e06
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-10-17 23:46:17 +0300

    635 Итоговый тест Гласные буквы
    Дано предложение, составленное из строчных букв латинского алфавита. Определить, сколько в нем гласных букв. Гласными считаются буквы  a, e, i, o, u.
    Входные данные
    На вход программе подается строка, длина которого не превосходит 1000.
    Выходные данные
    Выведите количество гласных букв.

---
+++ test635/test635/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace test635
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int sum = 0;
+            for (int i = 0; i <= s.Length-1; i++)
+            {
+                if (s[i] == 'a' || s[i] == 'A'||
+                    s[i] == 'e' || s[i] == 'E'||
+                    s[i] == 'i' || s[i] == 'I'||
+                    s[i] == 'o' || s[i] == 'O'||
+                    s[i] == 'u' || s[i] == 'U')
+                {
+                    sum++;
+                }
+                
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
