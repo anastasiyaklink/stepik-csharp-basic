01dc9a4868ca6aff9e063223069f20a1021886f2
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-10-18 00:07:13 +0300

    636 Итоговый тест Формирование строки
    Дано предложение, составленное из строчных букв латинского алфавита. Все буквы 'e' в нем заменить буквой 'i'.
    Входные данные
    На вход программе подается строка, длина которого не превосходит 1000.
    Выходные данные
    Выведите строку, в котором все буквы 'e' заменены буквой 'i'.

---
+++ test636/test636/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace test636
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            for (int i = 0; i <= s.Length - 1; i++)
+            {
+                Console.Write(s[i] != 'e' ? s[i]: 'i');
+                // if (s[i] != 'e') Console.Write(s[i]);
+                // else Console.Write("i");
+            }
+        }
+    }
+}
