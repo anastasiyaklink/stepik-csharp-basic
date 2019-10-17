4ee37c221cb62e7ae83c2a6b2880225d7c7fcf92
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-10-18 00:36:14 +0300

    637 Итоговый тест Цифры в тексте
    Дан текст, составленный из букв латинского алфавита и цифр. Напечатать все имеющиеся в нем цифры.
    Входные данные
    На вход программе подается текст, составленный из букв латинского алфавита и цифр, длина которого не превосходит 1000.
    Выходные данные
    Выведите все имеющие цифры в тексте через пробел.

---
+++ test637/test637/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace test637
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            for (int i = 0; i <= s.Length - 1; i++)
+            {
+                Console.Write (s[i] >= '0' && s[i] <= '9' ? s[i] + " " : "");
+            }
+        }
+    }
+}
