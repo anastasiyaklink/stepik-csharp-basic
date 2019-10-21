2f4e51f204b6cc2826244aca9b6f2c79ec6fab96
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-10-22 00:07:12 +0300

    633 Итоговый тест Инициалы
    По данным ФИО сформируйте строку, содержащую фамилию с инициалами. Например, по ‘Ivanov Ivan Ivanovich’ получите "Ivanov I.I.".
    Входные данные
    На вход программе подаются 3 строки - фамилия имя отчество.
    Выходные данные
    Выведите строку, содержащую фамилию с инициалами.

---
+++ test633/test633/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace test633
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string f = Console.ReadLine();
+            string i = Console.ReadLine();
+            string o = Console.ReadLine();
+            Console.Write(f + " " + i[0] + "." + o[0] + ".");
+
+        }
+    }
+}
