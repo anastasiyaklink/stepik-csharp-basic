f1cfacb056633f31d986538823e4d5da71ce29e1
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-10-21 23:58:25 +0300

    634 Итоговый тест Индексация в строке
    Даны два слова. Верно ли, что первое слово начинается на ту же букву, на которую заканчивается второе слово? Регистр букв имеет значение, то есть 'a' и 'A' считаются разными буквами.
    Входные данные
    На вход программе подаются 2 строки. Длины данных строк не превосходят 1000.
    Выходные данные
    Выведите "YES", если первое слово начинается на ту же букву, на которую заканчивается второе слово, иначе выведите "NO".

---
+++ test634/test634/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace test634
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string t = Console.ReadLine();
+            Console.WriteLine(s[0] == t[t.Length-1] ? "YES" : "NO");
+         
+        }
+    }
+}
