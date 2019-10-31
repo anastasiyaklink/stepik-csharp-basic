e23b941776d8e4f0abb672454681c8d69a3c5e69
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-10-31 23:36:51 +0300

    711 Одномерные массивы Поиск-1
    Дан массив, состоящий из целых чисел. Напишите программу, которая выведет элементы массива, индексы которых кратны трем (0, 3, 6...).
    Входные данные
    Сначала задано число 𝑁 — количество элементов в массиве (1≤𝑁≤100)
    Далее через пробел записаны N чисел — элементы массива. Массив состоит из целых чисел, каждое из которых по модулю не превышает 10^5.
    Выходные данные
    Необходимо вывести все элементы массива с номерами, кратными трем.

---
+++ program711/program711/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace program711
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            string s = Console.ReadLine();
+            string[] ss = s.Split(' ');
+            for (int i = 0; i < N; i++)
+            { 
+                if (i % 3 == 0) Console.Write(ss[i] + " ");
+            }
+        }
+    }
+}
+
