4039c9d728c6541278b14dbbba30c09999a248ab
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-11-01 01:43:10 +0300

    713  Одномерные массивы Кол-во элементов, больших предыдущего
    Дан массив, состоящий из целых чисел. Напишите программу, которая подсчитает количество элементов массива, больших предыдущего (элемента с предыдущим номером).
    Входные данные
    Сначала задано число 𝑁 — количество элементов в массиве (1≤𝑁≤100). Далее через пробел записаны 𝑁 чисел — элементы массива. Массив состоит из целых чисел, каждое из которых по модулю не превышает 10^9.
    Выходные данные
    Необходимо вывести единственное число - количество элементов массива, больших предыдущего.

---
+++ project713/project713/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace project713
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            string s = Console.ReadLine();
+            string[] ss = s.Split(' ');
+            int[] num = new int[N];
+            int sum = 0;
+            int x = Convert.ToInt32(Math.Pow(10, 9));
+            for (int i = 0; i < N; i++)
+            {
+                int number = Convert.ToInt32(ss[i]);
+                num[i] = number;
+
+                if (number > x)
+                {
+                    sum++;
+                }
+                x = number;
+            }
+            Console.Write(sum);
+        }
+    }
+}
