0b1b5bd9d69f53053dbf7f5d86b06d04f07aca50
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-11-02 01:57:32 +0300

    632 Итоговый тест Часть алфавита
    Даны две строчные буквы латинского алфавита. Выведите все строчные буквы латинского алфавита в алфавитном порядке, которые находятся между данными символами, а также их самих.
    Входные данные
    Вводятся две строчные буквы латинского алфавита.
    Выходные данные
    Выведите на одной строке через пробел все строчные буквы латинского алфавита в алфавитном порядке, которые находятся между данными символами, а также их самих.
    Примечание: первая буква не обязательно меньше, чем вторая буква.

---
+++ test632/test632/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace test632
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            char a = Console.ReadLine()[0];
+            char b = Console.ReadLine()[0];
+
+            for (int i = Math.Min(a, b); i <= Math.Max(a, b); i++)
+            {
+                char c = (char) i;
+                Console.Write(c + " ");
+            }
+        }
+    }
+}
