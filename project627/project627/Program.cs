27c6f71471d7de7b6fcb4646c71bd850a7ef472d
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-11-02 23:21:48 +0300

    627 Строки Идентификатор
    Определить, является ли введенное слово идентификатором, т.е. начинается ли оно с английской буквы в любом регистре или знака подчеркивания и не содержит других символов, кроме букв английского алфавита (в любом регистре), цифр и знака подчеркивания.
    Входные данные
    На вход программе подается одна строка.
    Выходные данные
    Если строка является идентификатором, необходимо вывести "YES", иначе - "NO".

---
+++ project627/project627/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace project627
+{
+    class MainClass
+    {
+        public static bool IsEnglishLetter(char ch)
+        {
+            return (ch >= 'a' && ch <= 'z')
+                || (ch >= 'A' && ch <= 'Z');
+        }
+
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            bool valid = s.Length > 0;
+            for (int i = 0; valid && i < s.Length; i++)
+            {
+                bool validForFirstChar = s[i] == '_' || IsEnglishLetter(s[i]);
+
+                if ((i == 0 && !validForFirstChar)
+                    || (i > 0 && !validForFirstChar && !char.IsDigit(s[i])))
+                {
+                    valid = false;
+                }
+            }
+            //bool valid = Regex.IsMatch(s, "^[_a-zA-Z][_\\w]*$");
+            Console.WriteLine(valid ? "YES" : "NO");
+        }
+    }
+}
