90aa8673486285878efc1c7d14579b16e101d400
Anastasiya Klink <anastasiya.klink@gmail.com>
2019-11-06 00:06:00 +0300

    714 Одномерные массивы Переставить соседние элементы
    Напишите программу, которая переставляет соседние элементы массива (1-й элемент поменять с 2-м, 3-й с 4-м и т.д. Если элементов нечетное число, то последний элемент остается на своем месте).
    Входные данные
    Сначала задано число 𝑁 — количество элементов в массиве (1≤𝑁≤35). Далее через пробел записаны 𝑁 чисел — элементы массива. Массив состоит из целых чисел, каждое из которых по модулю не превышает 10^6.
    Выходные данные
    Необходимо вывести массив, полученный после перестановки элементов.

---
+++ project714/project714/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace project714
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            string s = Console.ReadLine();
+            string[] ss = s.Split(' ');
+            int[] num = new int[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                int number = Convert.ToInt32(ss[i]);
+                num[i] = number;
+            }
+            if (N % 2 != 0)
+            {
+                N--;
+
+                for (int j = 0; j < N; j++)
+                {
+                    int x = num[j];
+                    num[j] = num[j + 1];
+                    num[j + 1] = x;
+                    Console.Write(num[j] + " " + num[j + 1] + " ");
+                    j++;
+                }
+                Console.Write(num[N]);
+            }
+            else
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    int x = num[j];
+                    num[j] = num[j + 1];
+                    num[j + 1] = x;
+                    Console.Write(num[j] + " " + num[j + 1] + " ");
+                    j++;
+                }
+            }
+        }
+    }
+}
